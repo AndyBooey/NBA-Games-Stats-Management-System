ed75a10b72453471920a9756848e3c7ba6b1663d
shawn-singharaj <140577720+shawn-singharaj@users.noreply.github.com>
2026-03-02 11:40:37 -0800

    Update Home.jsx
    Added reset button and onReset functionality

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,21 @@
-function Home() {
+function Home({ backendURL }) {
+    const onReset = async () =>{
+        try{
+            await fetch(`${backendURL}/Reset`, { method: 'POST'});
+            window.location.reload();
+            alert("Successfully reset database back to sample data.");
+        } catch(err){
+            console.error('Front: reset failed');
+        }
+    }
+
     return (
         <>
             <h1>NBA Games & Stats Management System</h1>
             <div className="homepageDescription">
                 <p>By: Shawn Singharaj & Andy Bui</p>
+                <h2>Reset to Original Sample Data:</h2>
+                <button onClick={onReset}>Reset</button>
             </div>
         </>
     )
